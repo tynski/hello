2fefbc84e7eed5ef0db995a7af3dc8a06b1aa40f
tyniacz <b.tynski@gmail.com>
2021-07-20 12:51:17 +0200

    greetings added

--- hello.go
+++ hello.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import ( 
+    "fmt"
 
-import "rsc.io/quote"
+    "github.com/tynski/greetings"
+)
+
 
 func main() {
-	fmt.Println(quote.Go())
+	// Get a greeting message and print it.
+	message := greetings.Hello("Barti")
+	fmt.Println(message)
 }
